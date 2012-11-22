5adc007a915994ff870084e08da9a321c36a519a
Jason Ling <jtg2078@gmail.com>
2012-11-22 23:42:56 +0800

    added code to reset badge count

--- sme/AppDelegate.m
+++ sme/AppDelegate.m
@@ -23,9 +23,12 @@
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
      [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
+    
     self.viewController = [[[ViewController alloc] initWithNibName:@"ViewController" bundle:nil] autorelease];
     self.window.rootViewController = self.viewController;
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
